d2932eb69594747572208d6c25b3bbbb0586ebea
Inhyeok Hwang <iam0manre@gmail.com>
2023-06-01 23:54:41 +0900

    and_gate_2룰 추가하였음

--- ch02_Perceptron/AND_gate.py
+++ ch02_Perceptron/AND_gate.py
@@ -3,10 +3,17 @@ x1과 x2라는 입력을 받아서 linear Classification을 통해서
 만약 값이 0.7보다 크면 1을 아니면 0을 출력하는 모델을 만들었습니다.
 '''
 
-def AND(x1, x2) :
-    w1, w2, theta = 0.5,0.5,0.7
+def and_gate(x1, x2) :
+    w1, w2, theta = 0.5, 0.5, 0.7
     tmp = x1 * w1 + x2 * w2
-    if tmp <=theta:
+    if tmp <= theta:
         return 0
     else:
         return 1
+
+
+print(and_gate(0, 0))
+print(and_gate(0, 1))
+print(and_gate(1, 0))
+print(and_gate(1, 1))
+
---
+++ ch02_Perceptron/and_gate_2.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+def and_gate_2(x1, x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(x*w) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+print(and_gate_2(1,0))
+
+def nand_gate(x1,x2):
+    x = np.array([x1,x2])
+    w = np.array([-0.5,-0.5])
+    b = 0.7
+    tmp = np.sum(x*w)+b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+def or_gate(x1,x2):
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(x * w) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+
+
