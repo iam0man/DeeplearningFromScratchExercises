954cbe6289f11f41ea40967b50d288d242c355d9
inhyeok h <iam0manre@gmail.com>
2023-06-06 12:57:15 +0900

    mnist 추가하기

---
+++ ch03_Neural Network/mnist_exercise.py
@@ -0,0 +1,7 @@
+import os, sys
+sys.path.append(os.pardir)
+from dataset.mnist import load_mnist
+
+(x_train, t_train), (x_test, t_test) = load_mnist(flatten =True, normalize = False)
+
+print(x_train.shape)
